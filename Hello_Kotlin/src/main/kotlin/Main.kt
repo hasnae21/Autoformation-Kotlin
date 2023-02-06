c8a727e98341ff9167c09d2201d503d567103fd7
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2023-02-06 11:54:25 +0100

    Lesson 1: Get started
    Lesson 1: Get started

--- Hello_Kotlin/src/main/kotlin/Main.kt
+++ Hello_Kotlin/src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Hello World From HASNAE!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
+
