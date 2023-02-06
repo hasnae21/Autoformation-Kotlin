7f0a7c27b314703b1f88d0d537ae28161f6c303a
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2023-02-06 11:59:49 +0100

    Lesson 1: Get started

--- Hello_Kotlin/src/main/kotlin/Main.kt
+++ Hello_Kotlin/src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello World From HASNAE!")
+    println("Hello World From HASNAE commited from Kotlin! ")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
