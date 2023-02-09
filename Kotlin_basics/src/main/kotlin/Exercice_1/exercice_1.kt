794daf31908a96c61eca858b3089e7efee7a219c
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2023-02-09 11:09:53 +0100

    exercice_1

--- Kotlin_basics/src/main/kotlin/Exercice_1/exercice_1.kt
+++ Kotlin_basics/src/main/kotlin/Exercice_1/exercice_1.kt
@@ -1,7 +1,16 @@
-fun main(){
-    print("Entrer le nombre de ligne voulu:")
-    val nbr_enter =Integer.valueOf(readLine())
+fun main() {
+    print("Entrer le nombre de ligne voulu: ")
+
+    val nbr_enter = readLine()!!.toInt()
     println("vous avez entrer $nbr_enter")
 
-
+    for (i in 1..nbr_enter) {
+        for (j in 1..nbr_enter - i) {
+            print(" ")
+        }
+        for (j in 1..(2 * i - 1)) {
+            print("*")
+        }
+        println()
+    }
 }
