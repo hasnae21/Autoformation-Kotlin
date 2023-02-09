8e9d2690d9c8fa54fe7edaf23107f36ee7cc18b7
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2023-02-09 13:52:16 +0100

    pyramide

--- Kotlin_basics/src/main/kotlin/Exercice_1/exercice_1.kt
+++ Kotlin_basics/src/main/kotlin/Exercice_1/exercice_1.kt
@@ -1,16 +1,18 @@
 fun main() {
-    print("Entrer le nombre de ligne voulu: ")
-
-    val nbr_enter = readLine()!!
+    println("Entrer le nombre de ligne voulu : ")
+    val nbr_enter = Integer.valueOf(readLine())
     println("vous avez entrer $nbr_enter")
 
     for (i in 1..nbr_enter) {
         for (j in 1..nbr_enter - i) {
             print(" ")
         }
+        print("_")
         for (j in 1..(2 * i - 1)) {
             print("*")
         }
+        print("_")
         println()
     }
+
 }
