e726a6a73e5ada62d8a2cdc68fe0ac4cb3480c66
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2023-02-20 10:32:39 +0100

    Create an interactive Dice Roller app
    Create an interactive Dice Roller app

--- DiceRoller/README.md
+++ DiceRoller/README.md
@@ -5,7 +5,6 @@
 > Comment afficher un message Toast
 > Mettre à jour le contenu d'un TextView pendant l'exécution de l'application
 
-
 // Solution:
 >> Ajoutez un élément Button dans une application Android à l'aide de l'éditeur de mise en page.
 >> Modifiez la classe MainActivity.kt pour ajouter un comportement interactif à l'application.
@@ -14,44 +13,3 @@
 >> Lorsque l'application est en cours d'exécution, vous pouvez mettre à jour ce qui s'affiche à l'écran en appelant des méthodes sur TextView, Button ou d'autres éléments d'UI dans la mise en page.
 >> Commentez votre code pour aider les personnes qui le liront à comprendre votre approche.
 Reformatez votre code et nettoyez-le.
-
-
-import android.os.Bundle
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
-
-class MainActivity : AppCompatActivity() {
-
-   override fun onCreate(savedInstanceState: Bundle?) {
-       super.onCreate(savedInstanceState)
-       setContentView(R.layout.activity_main)
-
-       val rollButton: Button = findViewById(R.id.button)
-       rollButton.setOnClickListener {
-           val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-           toast.show()       }
-   }
-}
-
-class MainActivity : AppCompatActivity() {
-
-   override fun onCreate(savedInstanceState: Bundle?) {
-       super.onCreate(savedInstanceState)
-       setContentView(R.layout.activity_main)
-
-       val rollButton: Button = findViewById(R.id.button)
-       rollButton.setOnClickListener {
-        Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
-        
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = "6"
-       }
-   }
-}
-
-class Dice(val numSides: Int) {
-
-   fun roll(): Int {
-       return (1..numSides).random()
-   }
-}
--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,6 +6,10 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)  //find button by it's id
         rollButton.setOnClickListener {     //onClick event
 
-        Toast.makeText(this, "dé lancer!", Toast.LENGTH_SHORT).show()  //validation button
+        Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()  //validation button
 
         //val resultTextView: TextView = findViewById(R.id.textView)  //find TextView by it's id
         //resultTextView.text = "6"  //afficher le text entre ""
@@ -25,13 +29,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
-        val dice = Dice(6)  // appel de class Dice()
 
-        val diceRoll = dice.roll()   // appel de fct roll()  //dice Roller is an Int
+        ///////////  Dice number 1
 
-        val resultTextView: TextView = findViewById(R.id.textView)  //find TextView by it's id
-        resultTextView.text = diceRoll.toString()  //afficher le text String entre ""
+        // Create first Dice object with 6 sides and roll it
+        val dice = Dice(6)  // appel 1 de class Dice()
+        val diceRoll = dice.roll()   // appel 1 de fct roll()  //dice Roller is an Int
+
+        // Update the screen with the dice roll 1
+        val resultTextView: TextView = findViewById(R.id.textView)  //find TextView by it's id textView
+        resultTextView.text = diceRoll.toString()  // afficher le text String entre ""
+
+
+        //////////  Dice number 2
+
+        val dice1 = Dice(6)
+        val diceRoll1 = dice.roll()
+
+        val resultTextView1: TextView = findViewById(R.id.textView1)
+        resultTextView1.text = diceRoll1.toString()
+
     }
 
 }
