6740cb19d189675577ab9c0d9b7ece8fe1611eb1
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2023-03-02 13:16:16 +0100

    save prototype_basic

--- ToDo_List/app/src/main/java/com/example/todo_list/MainActivity.kt
+++ ToDo_List/app/src/main/java/com/example/todo_list/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.todo_list
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val listView: ListView = findViewById(R.id.listView)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {addItem()}
+
+    }
+
+
+
+    private fun addItem() {
+        Toast.makeText(this,"btn clicked", Toast.LENGTH_SHORT).show()
     }
 }
